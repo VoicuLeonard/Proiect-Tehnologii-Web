c34d9e49378f60557b537717f73bf228ebad65d9
Voicu Leonard <leonardvoicu25@gmail.com>
2026-01-12 00:22:08 +0200

    Frontend: Implementare Dashboard Student/Profesor si integrare serviciu extern de citate

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -23,7 +23,7 @@ export const AuthProvider = ({ children }) => {
     // Configuram o instanta Axios globala cu URL-ul serverului
     // CORECT
     const api = axios.create({
-        baseURL: 'https://proiect-tehnologii-web-1.onrender.com/api', // <--- Am adaugat /api
+        baseURL: 'https://proiect-tehnologii-web-grbi.onrender.com/api', 
     });
 
     // Daca exista un token, il atasam automat la fiecare cerere HTTP (header Authorization)
